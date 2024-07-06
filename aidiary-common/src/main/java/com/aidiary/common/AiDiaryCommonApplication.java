36bfd5b084c1e08281456f45a3e197d6c04bdcf2
simdev1234 <simdev1234@gmail.com>
2024-07-06 21:49:33 +0900

    chore : Dockerfile, yml 파일 수정

---
+++ aidiary-common/src/main/java/com/aidiary/common/AiDiaryCommonApplication.java
@@ -0,0 +1,13 @@
+package com.aidiary.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDiaryCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDiaryCommonApplication.class, args);
+	}
+
+}
