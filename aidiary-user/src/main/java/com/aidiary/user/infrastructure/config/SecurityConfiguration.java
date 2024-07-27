f0785c282101ac744de8ec3d8a17ef62bd01182e
simdev1234 <simdev1234@gmail.com>
2024-07-27 18:49:21 +0900

    feat : cors 설정 추가

--- aidiary-user/src/main/java/com/aidiary/user/infrastructure/config/SecurityConfiguration.java
+++ aidiary-user/src/main/java/com/aidiary/user/infrastructure/config/SecurityConfiguration.java
@@ -20,6 +20,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.util.StringUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -33,9 +38,20 @@ public class SecurityConfiguration{
             "/api/v1/users/email/auth-code",
             "/api/v1/users/email/auth",
             "/api/v1/users/login",
-            "/api/v1/users/password",
-            "/api/v1/users/me"
+            "/api/v1/users/password"
     };
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://ai-diary-knou-front.vercel.app"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
